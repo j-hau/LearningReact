132ae4f6524848b5635a76ffd5f20db78a9d1596
j-hau <james.hau@me.com>
2020-06-25 17:14:59 +0100

    Added component.  Added comments

---
+++ src/Paragraph.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+{
+  /* need to import react for the component to work */
+}
+
+function Paragraph() {
+  {
+    /* defining the component paragraph */
+  }
+  return <p> This is a paragraph in a function </p>;
+}
+
+export default Paragraph;
+{
+  /*exporting it to be used as default.
+  Note that it is without parehthesis as it would export immediately */
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Paragraph from "./Paragraph";
+{
+  /* importing the Paragraph component above */
+}
+
+const text = "This is a suffix to test the addition of variables";
 
 ReactDOM.render(
   <div>
-    <h1>Hello World!</h1>
-    <p>This test script.</p>
+    <h1 className="heading" contentEditable="true">
+      {" "}
+      {/*This is setting the options for the header */}
+      Hello World!
+    </h1>
+    <u1>
+      <li>This test script. {text}</li>{" "}
+      {/* This is to test adding expressions in JSX */}
+      <li>
+        This is generating a random number: {Math.floor(Math.random() * 10)}
+      </li>
+    </u1>
+    <Paragraph />{" "}
+    {/*As there would be nothing that went between the two paragraph tags, I use a self closing tag */}
   </div>,
   document.getElementById("root")
 );
