82a62df6b21f874691d1a83c14439e0505cc1d4d
j-hau <james.hau@me.com>
2020-06-29 15:22:03 +0100

    Added component to test props in react app

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,24 @@ import pi, { twoPi, squarePi } from "./Constants";
   /* This is importing the default as pi, but also the extra exports defined in constants.js */
 }
 
+function ContactCard(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <img src={props.img} alt="avatar_img" />
+      <p>{props.tel}</p>
+      <p>{props.email}</p>
+    </div>
+  );
+}
+
+{
+  /*Above is creating a "template" that when passed an array of inputs
+  called 'props', it accesses individual elements and assigns them to
+  HTML tags to be returned and displayed 
+  It accesses the individual elements with expected names to assign them*/
+}
+
 function App() {
   return (
     <div>
@@ -22,6 +40,13 @@ function App() {
       {pi}
       <br />
       {twoPi()}
+      <ContactCard
+        name="Beyonce"
+        img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+        tel="111 111 111"
+        email="beyonce@beyonce.com"
+      />
+      {/* This is simply passing the data into the contact card function to be assigned */}
     </div>
   );
 }
